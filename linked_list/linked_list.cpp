4660070f3f92cafda0671fb6723e579f64f02224
Sourya Roy <souryaroy30@gmail.com>
2026-01-02 19:42:52 +0530

    formatted the code and the added a function to to check if an element is present in the linked list

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -1,52 +1,64 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-class Node{
-    public:
-    int data;
-    Node* next;
+class Node {
+public:
+  int data;
+  Node *next;
 
-    public:
-    Node(int data1, Node* next1){
-        data = data1;
-        next = next1;
-    }
+public:
+  Node(int data1, Node *next1) {
+    data = data1;
+    next = next1;
+  }
 
-    Node(int data1){
-        data = data1;
-        next = nullptr;
-    }
+  Node(int data1) {
+    data = data1;
+    next = nullptr;
+  }
 };
 
-Node* convertArrtoLL(vector<int> arr){
-    Node* head = new Node(arr[0]);
-    Node* mover = head;
-    for(int i = 1; i < arr.size(); i++){
-        Node* temp = new Node(arr[i]);
-        mover->next = temp;
-        mover = temp;
-    }
-    return head;
+Node *convertArrtoLL(vector<int> arr) {
+  Node *head = new Node(arr[0]);
+  Node *mover = head;
+  for (int i = 1; i < arr.size(); i++) {
+    Node *temp = new Node(arr[i]);
+    mover->next = temp;
+    mover = temp;
+  }
+  return head;
 }
 
-int lengthofLL(Node* head){
-    int count = 0;
-    while(head){
-        head = head->next;
-        count++;
-    }
-    return count;
+int lengthofLL(Node *head) {
+  int count = 0;
+  while (head) {
+    head = head->next;
+    count++;
+  }
+  return count;
 }
 
-int main(){
-    vector<int> arr = {12, 19, 21, 8, 14};
-    Node* head = convertArrtoLL(arr);
-    Node* temp = head;
-    while(temp){
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
+bool checkIfPresent(Node *head, int val) {
+  Node *temp = head;
+  while (temp) {
+    if (temp->data == val)
+      return true;
+    else
+      temp = temp->next;
+  }
+  return false;
+}
 
-    cout << endl << "length of the linked-list: " << lengthofLL(head) << endl;
+int main() {
+  vector<int> arr = {12, 19, 21, 8, 14};
+  Node *head = convertArrtoLL(arr);
+  Node *temp = head;
+  while (temp) {
+    cout << temp->data << " ";
+    temp = temp->next;
+  }
+
+  cout << endl << "length of the linked-list: " << lengthofLL(head) << endl;
+  cout << "Is 8 present in the linked-list: " << checkIfPresent(head, 9);
 }
