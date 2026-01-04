b1497c880eaa1212d98188c273a41cb1c475ac88
Sourya Roy <souryaroy30@gmail.com>
2026-01-04 16:29:28 +0530

    added deleteK function

--- doubly_linked_list/doubly_linked_list.cpp
+++ doubly_linked_list/doubly_linked_list.cpp
@@ -66,19 +66,50 @@ Node* removeTail(Node* head) {
     while(tail->next != NULL) {
         tail = tail->next;
     }
-    Node* prev = tail->back;
+    Node* newTail = tail->back;
     tail->back = nullptr;
-    prev->next = nullptr;
+    newTail->next = nullptr;
     delete tail;
     return head;
 }
 
+int countNode(Node* head) {
+    int count = 0;
+    Node* temp = head;
+    while(temp) {
+        count++;
+        temp = temp->next;
+    }
+
+    return count;
+}
+
+Node* removeKth(Node* head, int k) {
+    if(head == NULL || k <= 0) return NULL;
+    if(k == 1) return removeHead(head);
+    Node* temp = head;
+    int cnt = 1;
+
+    while(temp != NULL) {
+        if(cnt == k) {
+            if(temp->next == NULL) return removeTail(head);
+            temp->back->next = temp->next;
+            temp->next->back = temp->back;
+            delete temp;
+            break;
+        }
+        temp = temp->next;
+        cnt++;
+    }
+    return head;
+}
+
+
 int main(){
-    vector<int> arr = {2, 3, 6, 7, 8};
+    vector<int> arr = {2, 4, 6, 7, 8};
     Node* head = arrToDLL(arr);
     print(head);
-
-    head = removeTail(head);
+    head = removeKth(head, 1);
     print(head);
     return 0;
 }
